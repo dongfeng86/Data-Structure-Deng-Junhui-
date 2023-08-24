926acb221e06301b7add75b60b94c4432a12945b
unknown <115949168@qq.com>
2023-08-24 18:25:19 +0800

    2023.8.24 from office

--- huffman/Huffman_PQ_Test.cpp
+++ huffman/Huffman_PQ_Test.cpp
@@ -13,23 +13,33 @@
  * 编码森林的实现方式采用优先级队列时，编译前对应的工程只需设置相应标志：
  *    DSA_PQ_List、DSA_PQ_ComplHeap或DSA_PQ_LeftHeap
  ******************************************************************************************/
-int main ( int argc, char* argv[] ) { //Huffman编码算法统一测试
-   /*DSA*/if ( 3 > argc ) { printf ( "Usage: %s <sample-text-file> <message#1> [message#2] ...\a\n", argv[0] ); return -1; }
-   int* freq = statistics ( argv[1] ); //根据样本文件，统计各字符的出现频率
-   HuffForest* forest = initForest ( freq ); release ( freq ); //创建Huffman森林
-   HuffTree* tree = generateTree ( forest ); release ( forest ); //生成Huffman编码树
-   /*DSA*/print ( tree ); //输出编码树
-   HuffTable* table = generateTable ( tree ); //将Huffman编码树转换为编码表
-   /*DSA*/for ( int i = 0; i < N_CHAR; i++ ) //输出编码表
-      /*DSA*/printf ( " %c: %s\n", i + 0x20, * ( table->get ( i + 0x20 ) ) );
-   for ( int i = 2; i < argc; i++ ) { //对于命令行传入的每一明文串
-      /*DSA*/printf ( "\nEncoding: %s\n", argv[i] ); //以下测试编码
-      Bitmap* codeString = new Bitmap; //二进制编码串
-      int n = encode ( table, codeString, argv[i] ); //将根据编码表生成（长度为n）
-      /*DSA*/printf ( "%s\n", codeString->bits2string ( n ) ); //输出该编码串
-      /*DSA*/printf ( "Decoding: " ); //以下测试解码
-      decode ( tree, codeString, n ); //利用Huffman编码树，对长度为n的二进制编码串解码
-      release ( codeString );
-   }
-   release ( table ); release ( tree ); return 0; //释放编码表、编码树
+int main(int argc, char* argv[])
+{ //Huffman编码算法统一测试
+	if (3 > argc)
+	{
+		printf("Usage: %s <sample-text-file> <message#1> [message#2] ...\a\n", argv[0]);
+		return -1;
+	}
+
+	int* freq = statistics(argv[1]); //根据样本文件，统计各字符的出现频率
+	HuffForest* forest = initForest(freq); release(freq); //创建Huffman森林
+	HuffTree* tree = generateTree(forest); release(forest); //生成Huffman编码树
+	print(tree); //输出编码树
+	HuffTable* table = generateTable(tree); //将Huffman编码树转换为编码表
+
+	for (int i = 0; i < N_CHAR; i++) //输出编码表
+		printf(" %c: %s\n", i + 0x20, *(table->get(i + 0x20)));
+
+	for (int i = 2; i < argc; i++) 
+	{ //对于命令行传入的每一明文串
+		printf("\nEncoding: %s\n", argv[i]); //以下测试编码
+		Bitmap* codeString = new Bitmap; //二进制编码串
+		int n = encode(table, codeString, argv[i]); //将根据编码表生成（长度为n）
+		printf("%s\n", codeString->bits2string(n)); //输出该编码串
+		printf("Decoding: "); //以下测试解码
+		decode(tree, codeString, n); //利用Huffman编码树，对长度为n的二进制编码串解码
+		release(codeString);
+	}
+
+	release(table); release(tree); return 0; //释放编码表、编码树
 } //release()负责释放复杂结构，与算法无直接关系，具体实现详见代码包
--- huffman/huffChar.h
+++ huffman/huffChar.h
@@ -9,8 +9,10 @@
 #pragma once
 
 #define  N_CHAR  (0x80 - 0x20) //仅以可打印字符为例
-struct HuffChar { //Huffman（超）字符
-   char ch; int weight; //字符、频率
+struct HuffChar		//Huffman（超）字符
+{ 
+   char ch;		//字符
+   int weight;	//频率
    HuffChar ( char c = '^', int w = 0 ) : ch ( c ), weight ( w ) {};
 // 比较器、判等器（各列其一，其余自行补充）
    bool operator< ( HuffChar const& hc ) { return weight > hc.weight; } //此处故意大小颠倒
